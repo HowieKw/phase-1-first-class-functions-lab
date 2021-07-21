3927e5e3221dee28b91c50d49f4e098536cf9857
Howard Kwon <howie.ykwon70@gmail.com>
2021-07-21 12:20:48 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2,4);
+}
+
+
+const selectingDrivers = 
+[returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier (num) {
+    return function(fare) {
+        return num*fare;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+
+function selectDifferentDrivers(array, returnTwoDrivers) {
+    return returnTwoDrivers(array);
+}
